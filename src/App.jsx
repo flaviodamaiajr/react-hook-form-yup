1be1848da19617f9aa1a561b1e9ef0d512e33655
Flávio Antônio da Maia Júnior <flaviodamaiajr@gmail.com>
2023-07-27 09:16:05 -0300

    feat: Change TextField component to use Controller.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import { FieldLabel } from './components/FieldLabel'
+import { TextField } from './components/TextField'
 
 const validationSchema = yup.object().shape({
   name: yup.string().required('Name is required'),
@@ -31,53 +31,9 @@ const App = () => {
   return (
     <div className='container'>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="name"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <FieldLabel
-              label="Name"
-              name={field.name}
-              value={field.value}
-              onChange={field.onChange}
-              onBlur={field.onBlur}
-              error={errors[field.name]?.message}
-            />
-          )}
-        />
-
-        <Controller
-          name="email"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <FieldLabel
-              label="Email"
-              name={field.name}
-              value={field.value}
-              onChange={field.onChange}
-              onBlur={field.onBlur}
-              error={errors[field.name]?.message}
-            />
-          )}
-        />
-
-        <Controller
-          name="age"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <FieldLabel
-              label="Age"
-              name={field.name}
-              value={field.value}
-              onChange={field.onChange}
-              onBlur={field.onBlur}
-              error={errors[field.name]?.message}
-            />
-          )}
-        />
+        <TextField label="Name" name="name" placeholder="Enter your name" control={control} error={errors.name?.message} />
+        <TextField label="Email" name="email" placeholder="Enter your email" control={control} error={errors.email?.message} />
+        <TextField label="Age" name="age" placeholder="Enter your age" control={control} error={errors.age?.message} />
         <div style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
           <button type="submit" className='btn btn-primary'>Save</button>
           <button type="button" className='btn btn-warning' onClick={handleClear}>Clear</button>
---
+++ src/components/TextField.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Controller } from 'react-hook-form';
+
+export const TextField = ({ label, name, placeholder, error, onChange, onBlur, value, control, ...rest }) => {
+
+console.log(error);
+
+    return (
+        <div className={`form-group pt-2 ${error ? 'has-error' : ''}`}>
+            <label>{label}</label>
+            <Controller
+                name={name}
+                control={control}
+                render={({ field }) => (
+                    <div>
+                        <input
+                            className={`form-control ${error ? 'is-invalid' : ''}`}
+                            type="text"
+                            id={field.name}
+                            name={field.name}
+                            placeholder={placeholder}
+                            value={field.value}
+                            onChange={field.onChange}
+                            onBlur={field.onBlur}
+                            {...rest}
+                        />
+                        {error ?
+                            <div className="invalid-feedback">
+                                * {error}
+                            </div>
+                            : null}
+                    </div>
+                )}
+            />
+        </div>
+    );
+};
